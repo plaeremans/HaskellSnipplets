966dac86f2f053d65d5ce7897de1a61def33310c
Pieter Laeremans <pieter@laeremans.org>
2010-08-10 01:09:03 +0200

    Initial ziplist tool added

---
+++ ZipList.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import System
+import System.IO
+
+import qualified Data.ByteString.Char8 as B
+import Data.Char
+import Data.Set 
+import Codec.Archive.Zip
+
+main = do
+  args <- getArgs
+  if ((length args) < 1) 
+     then  putStrLn "You should provide a  file as argument" 
+     else  
+      do 
+        let arg1:_ = args 
+        bytes <- B.readFile arg1
+	let ar = toArchive bytes
+	let cdc = eRelativePath $ head $ zEntries ar	
+	putStrLn $ show cdc
+        return ()
